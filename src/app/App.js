32141b59c66fd2baae3aef8f0028bcc85cacd1d8
Roman Makovyak <romanmakovyak@Romans-MBP.fritz.box>
2018-10-22 18:09:00 +0200

    re

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import styleguide from 'styleguide/partials/_box.scss';
 
-import styles from './App.scss';
-
-const App = () => (
-  <div>
-    <h1 className={styles.App}>Hello world</h1>
-    <input className={styleguide.input} />
-    <div className={styleguide.box}>
-      <span>Custom label</span>
-    </div>
-  </div>
-);
+const App = () => <div>Hello</div>;
 
 export default hot(module)(App);
